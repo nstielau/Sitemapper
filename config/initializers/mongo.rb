2f10d58620fe4371dcc8420efa9e709bb101d6f6
Nick Stielau <nick.stielau@gmail.com>
2010-08-28 22:30:07 -0700

    Handling mongo 1.6 issue with driver.

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -3,13 +3,13 @@ require 'mongo_mapper'
 APPLICATION_NAME="site_graph"
 
 if Rails.env == "development"
-  MongoMapper.connection = Mongo::Connection.new
+  MongoMapper.connection = Mongo::Connection.new("127.0.0.1", 27017, :slave_ok => true)
   MongoMapper.database = "#{APPLICATION_NAME}_development"
 elsif Rails.env == "test"
-  MongoMapper.connection = Mongo::Connection.new
+  MongoMapper.connection = Mongo::Connection.new("127.0.0.1", 27017, :slave_ok => true)
   MongoMapper.database = "#{APPLICATION_NAME}_test"
 else
-  MongoMapper.connection = Mongo::Connection.new("flame.mongohq.com", ENV['MONGOHQ_PORT'])
+  MongoMapper.connection = Mongo::Connection.new("flame.mongohq.com", ENV['MONGOHQ_PORT'], :slave_ok => true)
   MongoMapper.database = APPLICATION_NAME
   MongoMapper.database.authenticate(ENV['MONGOHQ_USERNAME'], ENV['MONGOHQ_PASSWORD'])
 end
