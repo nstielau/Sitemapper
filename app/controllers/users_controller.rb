0e6507e080ca14dde6a69a48f9d2e4dcd4c05d48
Nick Stielau <nick.stielau@gmail.com>
2010-09-20 09:50:12 -0700

    Cleaning up, redirecting to new graph page after sucessful auth.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,7 @@ class UsersController < ApplicationController
 
   def request_seomoz_authorization
     params_to_sign = {:AccessID => SEOMOZ_ACCESS_ID, :Expires => Time.now.to_i + 180, :Redirect => CGI::escape("http://#{request.env["HTTP_HOST"]}/users/authorization/accept")}
-    Rails.logger.info("Params to sign: #{params_to_sign.inspect}")
     params_to_sign[:Signature] = Linkscape::Signer.signParams(params_to_sign, [:AccessID, :Expires, :Redirect], SEOMOZ_SECRET_KEY)
-    #host = "ec2-184-73-17-79.compute-1.amazonaws.com"
     host = "apidash.seomoz.org"
     redirect_to "http://#{host}/delegate?AccessID=#{params_to_sign[:AccessID]}&Expires=#{params_to_sign[:Expires]}&Signature=#{params_to_sign[:Signature]}&Redirect=#{params_to_sign[:Redirect]}"
   end
@@ -39,6 +37,6 @@ class UsersController < ApplicationController
     current_user.has_delegated_seomoz_auth = true
     current_user.save
     flash[:notice] = "Authorization complete."
-    redirect_to "/"
+    redirect_to "/graphs/new"
   end
 end
